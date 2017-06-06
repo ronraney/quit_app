7449d0dcec241a8034dc83c85b9b1a7eafa6ca51
Ronnie <raneyron@yahoo.com>
2017-06-06 11:02:56 -0500

    Added view and template for detailed quitting object

--- quit/urls.py
+++ quit/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.something_list, name='something_list'),
+    url(r'^thing/(?P<pk>\d+)/$', views.thing_detail, name='thing_detail'),
 ]
--- quit/views.py
+++ quit/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Thing
 
@@ -6,3 +6,8 @@ from .models import Thing
 def something_list(request):
     things = Thing.objects.filter(quit_date__lte=timezone.now()).order_by('quit_date')
     return render(request, 'quit/something_list.html', {'things': things})
+
+def thing_detail(request, pk):
+    thing = get_object_or_404(Thing, pk=pk)
+    return render(request, 'quit/thing_detail.html', {'thing': thing})
+    
