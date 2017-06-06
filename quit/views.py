a2b70c599b3668452c79997f0d533ff3c8694ab3
Ronnie <raneyron@yahoo.com>
2017-06-06 09:34:53 -0500

    Modified templates to display things from database

--- quit/views.py
+++ quit/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Thing
 
 # Create your views here.
 def something_list(request):
-    return render(request, 'quit/something_list.html', {})
+    things = Thing.objects.filter(quit_date__lte=timezone.now()).order_by('quit_date')
+    return render(request, 'quit/something_list.html', {'things': things})
